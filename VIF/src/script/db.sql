5ace6a6f885c12fb41388c4b1a07027f46f99d86
nhannguyen86 <nguyenthanhnhan862005@gmail.com>
2015-06-12 13:43:06 +0700

    -create admin user script

---
+++ VIF/src/script/db.sql
@@ -0,0 +1,5 @@
+
+/* Data for the `user` table  (Records 1 - 1): admin - pass:123456 */
+
+INSERT INTO `user` (`ID`, `BIRTHDAY`, `CREATED_BY`, `CREATED_DATE`, `EMAIL`, `FULL_NAME`, `LOCKED`, `PASSWORD`, `ROLE`, `UPDATED_BY`, `UPDATED_DATE`, `USER_NAME`) VALUES 
+  (1, NULL, NULL, NULL, NULL, 'Admin', False, '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'ADMIN', NULL, NULL, 'vif');
