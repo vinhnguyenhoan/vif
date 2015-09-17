ce5b93b61caa854e3c4718de91b221d500fda074
nhannguyen86 <nguyenthanhnhan862005@gmail.com>
2015-09-17 12:07:29 +0700

    -merge from Vinh -add date filter

--- VIF/src/main/java/vn/vif/models/filter/OrderListFilter.java
+++ VIF/src/main/java/vn/vif/models/filter/OrderListFilter.java
@@ -1,20 +1,45 @@
 package vn.vif.models.filter;
 
+import java.util.Date;
+
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.type.StandardBasicTypes;
 
 import vn.vif.daos.DistinctFilter;
 import vn.vif.models.OrderList;
+import vn.vif.utils.VIFUtils;
 
 public class OrderListFilter implements DistinctFilter {
 	private String searchText;
+	private Date date;
 //	private List<Long> searchDistrict;
 	
 	public String getSearchText() {
 		return searchText;
 	}
 
+	public Date getDateValue() {
+		if (this.date == null) {
+			this.date = VIFUtils.convertStringToDate(
+					VIFUtils.formatDate(new Date()), "dd/MM/yyyy");// truncate date
+		}
+		return date;
+	}
+	
+	public String getDate() {
+		if (this.date != null) {
+			return VIFUtils.formatDate(this.date);// truncate date
+		}
+		return VIFUtils.formatDate(new Date());
+	}
+
+	public void setDate(String date) {
+		this.date = VIFUtils.convertStringToDate(date, "dd/MM/yyyy");
+	}
+	
 	public void setSearchText(String searchText) {
 		this.searchText = searchText;
 	}
@@ -41,6 +66,8 @@ public class OrderListFilter implements DistinctFilter {
 //			districtDis.add(Restrictions.in("districtId", searchDistrict));
 //			criteria.add(districtDis);
 //		}
+		
+		criteria.add(Restrictions.sqlRestriction(" date(ordered_date) = date(?) ", getDateValue(), StandardBasicTypes.DATE));
 		
 		criteria.addOrder(Order.desc("id"));
 		return criteria;
