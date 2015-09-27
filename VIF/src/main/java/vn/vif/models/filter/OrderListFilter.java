4a5373ffc2b0f05ee422716f8d6938ee063a01fe
vinhhnguyen <vinhhnguyen@kms-technology.com>
2015-09-27 23:46:03 +0700

    Filter order list from date range

--- VIF/src/main/java/vn/vif/models/filter/OrderListFilter.java
+++ VIF/src/main/java/vn/vif/models/filter/OrderListFilter.java
@@ -2,10 +2,12 @@ package vn.vif.models.filter;
 
 import java.util.Date;
 
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.type.DateType;
 import org.hibernate.type.StandardBasicTypes;
 
 import vn.vif.daos.DistinctFilter;
@@ -14,30 +16,51 @@ import vn.vif.utils.VIFUtils;
 
 public class OrderListFilter implements DistinctFilter {
 	private String searchText;
-	private Date date;
+	private Date fromOrderDate;
+	private Date toOrderDate;
+
 //	private List<Long> searchDistrict;
 	
 	public String getSearchText() {
 		return searchText;
 	}
 
-	public Date getDateValue() {
-		if (this.date == null) {
-			this.date = VIFUtils.convertStringToDate(
+	public Date getFromDateValue() {
+		if (this.fromOrderDate == null) {
+			this.fromOrderDate = VIFUtils.convertStringToDate(
 					VIFUtils.formatDate(new Date()), "dd/MM/yyyy");// truncate date
 		}
-		return date;
+		return fromOrderDate;
 	}
 	
-	public String getDate() {
-		if (this.date != null) {
-			return VIFUtils.formatDate(this.date);// truncate date
+	public String getFromOrderDate() {
+		if (this.fromOrderDate != null) {
+			return VIFUtils.formatDate(this.fromOrderDate);// truncate date
 		}
 		return VIFUtils.formatDate(new Date());
 	}
 
-	public void setDate(String date) {
-		this.date = VIFUtils.convertStringToDate(date, "dd/MM/yyyy");
+	public void setFromOrderDate(String date) {
+		this.fromOrderDate = VIFUtils.convertStringToDate(date, "dd/MM/yyyy");
+	}
+	
+	public Date getToDateValue() {
+		if (this.toOrderDate == null) {
+			this.toOrderDate = VIFUtils.convertStringToDate(
+					VIFUtils.formatDate(new Date()), "dd/MM/yyyy");// truncate date
+		}
+		return toOrderDate;
+	}
+	
+	public String getToOrderDate() {
+		if (this.toOrderDate != null) {
+			return VIFUtils.formatDate(this.toOrderDate);// truncate date
+		}
+		return VIFUtils.formatDate(new Date());
+	}
+
+	public void setToOrderDate(String date) {
+		this.toOrderDate = VIFUtils.convertStringToDate(date, "dd/MM/yyyy");
 	}
 	
 	public void setSearchText(String searchText) {
@@ -55,19 +78,14 @@ public class OrderListFilter implements DistinctFilter {
 	public Criteria getCriteria(Session session) {
 		Criteria criteria = session.createCriteria(OrderList.class);
 		
-//		if (StringUtils.isNotEmpty(searchText)) {
-//			criteria.add(Restrictions.disjunction()
-//					.add(Restrictions.ilike("customer.name", "%" + searchText + "%"))
-//					);
-//		}
+		if (StringUtils.isNotEmpty(searchText)) {
+			Criteria customerCriteria = criteria.createCriteria("customer");
+			customerCriteria.add(Restrictions.like("name", "%" + searchText + "%"));
+		}
 
-//		if (searchDistrict != null) {
-//			Disjunction districtDis = Restrictions.disjunction();
-//			districtDis.add(Restrictions.in("districtId", searchDistrict));
-//			criteria.add(districtDis);
-//		}
-		
-		criteria.add(Restrictions.sqlRestriction(" date(ordered_date) = date(?) ", getDateValue(), StandardBasicTypes.DATE));
+		criteria.add(Restrictions.sqlRestriction(" date(ordered_date) >= date(?) and date(ordered_date) <= date(?)", 
+								new Object[]{getFromDateValue(), getToDateValue()}, 
+								new DateType[]{StandardBasicTypes.DATE, StandardBasicTypes.DATE}));
 		
 		criteria.addOrder(Order.desc("id"));
 		return criteria;
